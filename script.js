a23d55dad6b24cbf97851a08f618b0fe60001ac4
conectavele <conectavel@gmail.comm>
2023-05-13 22:23:53 -0300

    Atualização de estrutura htm e estilização css

---
+++ script.js
@@ -0,0 +1,8 @@
+const navegaTabImg = document.querySelector(".animais-lista");
+//console.log(navegaTabImg.children);
+const navegaTabTxt = document.querySelector(".animais-descricao");
+//console.log(navegaTabTxt.children);
+const lista = navegaTabImg.children;
+lista.addEventListener("click", function (item, index) {
+  lista.classList.remove[item];
+}) 
