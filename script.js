00e61c713b206faae7e29eb422b7fc32882cb2e9
conectavele <conectavel@gmail.comm>
2023-05-15 01:01:17 -0300

    Some message

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
-const navegaTabImg = document.querySelectorAll(".js-tabmenu li");
-//console.log(navegaTabImg);
-const navegaTabTxt = document.querySelectorAll(".js-tabcontent section");
-//console.log(navegaTabTxt[1]);
+function navTab() {
+  /* Abaixo temo um códido de navegação por tab, ou seja, com evento click, adiciona um class no css "ativo" de acordo com evento click, relacionando os indices da NodeList.*/
+  const navegaTabImg = document.querySelectorAll(".js-tabmenu li");
+  //console.log(navegaTabImg);
+  const navegaTabTxt = document.querySelectorAll(".js-tabcontent section");
+  //console.log(navegaTabTxt[1]);
 
-function activeTab(index) {
-  navegaTabTxt[index].classList.add("ativo");
-  navegaTabTxt.forEach((section) => {
-    section.classList.remove("ativo");
+  function activeTab(index) {
+    navegaTabTxt[index].classList.add("ativo");
+    navegaTabTxt.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    navegaTabTxt[index].classList.add("ativo");
+  };
+  navegaTabImg.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      activeTab(index);
+    });
   });
-  navegaTabTxt[index].classList.add("ativo");
-};
-navegaTabImg.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    activeTab(index);
-  });
+}
+navTab() //isolando a função
+
+
+
+const imgTop = document.querySelector('img');
+console.log(imgTop.offsetTop)
+
+const imgTopwidth = document.querySelectorAll('img');
+let soma = 0;
+imgTopwidth.forEach(function (item, index, array) {
+  soma += item.offsetWidth
 });
+console.log(soma);
+
+const links = document.querySelectorAll('a');
+links.forEach(function (item) {
+  const linkW = item.offsetWidth;
+  const linkH = item.offsetHeight;
+  if (linkW >= 48 && linkH >= 48) {
+    console.log(item, "Possuí acessibilidade")
+  } else {
+    console.log(item, "Não possui acessibilidade")
+  }
+})
+
+const browserSmall = window.matchMedia("(max-width: 720px)").matches;
+console.log(browserSmall)
+if (browserSmall) {
+  const menu = document.querySelector(".menu")
+  menu.classList.add("mobile")
+}
